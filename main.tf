900c106ad98e4d514d54f5cb9e2bfb899b997604
Andrew Graham <andrew@firehawkvfx.com>
2020-11-08 16:45:30 +1030

    public_subnets

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "aws_route" "public_gateway" {
 
 resource "aws_route_table_association" "private_associations" {
   depends_on = [ aws_vpc.main, aws_subnet.private_subnet ]
-  count = var.create_vpc ? length( local.private_subnets ) : 0
+  count = var.create_vpc ? length( var.public_subnets ) : 0
 
   subnet_id      = element( aws_subnet.private_subnet.*.id, count.index )
   route_table_id = element( aws_route_table.private.*.id, 0 )
@@ -168,7 +168,7 @@ resource "aws_route_table_association" "private_associations" {
 
 resource "aws_route_table_association" "public_associations" {
   depends_on = [ aws_vpc.main, aws_subnet.public_subnet ]
-  count = var.create_vpc ? length( local.public_subnets ) : 0
+  count = var.create_vpc ? length( var.public_subnets ) : 0
 
   subnet_id      = element( aws_subnet.public_subnet.*.id, count.index )
   route_table_id = element( aws_route_table.public.*.id, 0 )
