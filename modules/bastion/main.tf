a126c91285bba2619e61d0303c5a98d1aee4c01b
Andrew Graham <andrew@firehawkvfx.com>
2021-01-24 10:30:36 +1030

    sleep before wake

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -199,7 +199,7 @@ resource "null_resource" "start-bastion" {
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = "aws ec2 start-instances --instance-ids ${local.id}"
+    command = "sleep 10; aws ec2 start-instances --instance-ids ${local.id}"
   }
 }
 
