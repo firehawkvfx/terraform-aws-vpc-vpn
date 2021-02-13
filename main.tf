c135cc5e86964aeac9ddce7a20a221e4202d9d4a
Andrew Graham <andrew@firehawkvfx.com>
2021-02-13 11:28:11 +1030

    fix fqdn

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ resource "aws_vpc" "main" {
 
 resource "aws_vpc_dhcp_options" "main" {
   count       = var.create_vpc ? 1 : 0
-  domain_name          = var.private_domain # This may not be available to be customised for us-east-1
+  # domain_name          = var.private_domain # This may not be available to be customised for us-east-1
   # domain_name_servers  = ["AmazonProvidedDNS"]
-  domain_name_servers  = ["127.0.0.1", "AmazonProvidedDNS"] # Annoyingly "127.0.0.53" must be provided for ubuntu
+  domain_name_servers  = ["127.0.0.1", "AmazonProvidedDNS"]
   ntp_servers          = ["127.0.0.1"]
   netbios_name_servers = ["127.0.0.1"]
   netbios_node_type    = 2
